948f1ae83150f820951b7184c45dd416e3daf85c
Atsuki3145 <mizutama430blue@outlook.jp>
2025-09-10 14:36:32 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,21 +1,25 @@
-const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with Pong!"),
-].map(command => command.toJSON());
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log("⏳ コマンド登録中...");
+    console.log('⏳ コマンド登録中...');
+
     await rest.put(
       Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-      { body: commands }
+      { body: commands },
     );
-    console.log("✅ コマンド登録完了！");
+
+    console.log('✅ コマンド登録完了！');
   } catch (error) {
     console.error(error);
   }
